ef6145e28cfdb19e1b110e37095b8fdce3710194
Will Strinz <wstrinz@gmail.com>
2018-06-11 12:46:36 -0500

    render inside ion content

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,6 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img, node)
-import Html.Attributes exposing (src, attribute)
+import Html exposing (Html, div, h1, img, node, text)
 import Html.Events exposing (onClick)
 
 
@@ -41,43 +40,14 @@ update msg model =
             { model | counter = model.counter - 1 } ! []
 
 
-
----- VIEW ----
-
-
 view : Model -> Html Msg
 view model =
     div []
-        [ Html.p [] [ text "Elm with ionic buttons!" ]
+        [ Html.p [] [ text "Elm is here!" ]
         , node "ion-button" [ onClick Increment ] [ text "+" ]
         , node "ion-button" [ onClick Decrement ] [ text "-" ]
         , Html.p [] [ text <| "Count is " ++ toString model.counter ]
         ]
-
-
-
--- fullIonicView : Model -> Html Msg
--- fullIonicView model =
---     div []
---         [ node "ion-app"
---             []
---             [ node "ion-header"
---                 []
---                 [ node "ion-toolbar"
---                     [ attribute "color" "primary" ]
---                     [ node "ion-title" [] [ text "Elmonic!" ]
---                     ]
---                 ]
---             , node "ion-content"
---                 [ attribute "padding" "" ]
---                 [ Html.p [] [ text "Elm is here!" ]
---                 , node "ion-button" [ onClick Increment ] [ text "+" ]
---                 , Html.p [] [ text <| toString model.counter ]
---                 ]
---             , Html.p [] [ text <| toString model.counter ]
---             ]
---         , Html.p [] [ text <| toString model.counter ]
---         ]
 
 
 main : Program Never Model Msg
