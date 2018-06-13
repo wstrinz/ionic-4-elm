db9357eae508d6f075a6229b9c225e84bede1410
Will Strinz <wstrinz@gmail.com>
2018-06-13 15:35:37 -0500

    don't load ionic core from cdn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import './main.css';
-import { Main } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
+import "./main.css";
+import { Main } from "./Main.elm";
+import registerServiceWorker from "./registerServiceWorker";
+import "@ionic/core/dist/ionic";
 
-Main.embed(document.getElementById('root'));
+Main.embed(document.getElementById("root"));
 
 registerServiceWorker();
