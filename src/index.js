ceb9d849817254158abae727fc36e84758175b06
Will Strinz <wstrinz@gmail.com>
2018-06-25 13:20:26 -0500

    add capacitor include for web

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./main.css";
 import { Main } from "./Main.elm";
+import { Capacitor } from "@capacitor/core";
 import registerServiceWorker from "./registerServiceWorker";
 import "@ionic/core/dist/ionic";
 
